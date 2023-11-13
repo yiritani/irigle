098ef6cc3fe94d323b9f451644391ba674dbaac2
yiritani <74131902+yiritani@users.noreply.github.com>
2023-11-13 22:06:41 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { HomeHeader } from '@/components/HomeHeader'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <HomeHeader />
+    </>
+
 
   )
 }
--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Link from "next/link";
+import {PiDotsNineBold} from "react-icons/pi";
 
 type Props = {}
 
-function Header(props: Props) {
+export function HomeHeader(props: Props) {
   return (
-    <div></div>
+    <header className='flex justify-end p-5 text-sm'>
+      <div className={'flex space-x-4 items-center'}>
+        <Link href={'https://mail.google.com'} className={'hover:underline items-center '}>
+          Gmail
+        </Link>
+        <Link href={'https://image.google.com'} className={'hover:underline items-center'}>
+          Images
+        </Link>
+        <PiDotsNineBold 
+        className={'h-6 w-6 text-gray-500 hover:bg-gray-200 rounded-full text-4xl items-center '}/>
+        <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 transition-shadow'>Sign in</button>
+      </div>
+    </header>
   );
 }
-
-export default Header;
